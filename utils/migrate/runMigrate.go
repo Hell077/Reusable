271ea84d1cfb15b3093545751883528bf62b964f
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-10-24 13:52:07 +0500

    update migrations functions

--- utils/migrate/runMigrate.go
+++ utils/migrate/runMigrate.go
@@ -4,10 +4,37 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-func RunMigrate(path string) error {
+func findMigrationsPath(name string) (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("не удалось получить текущий каталог: %w", err)
+	}
+
+	for {
+		migrationsPath := filepath.Join(dir, name)
+		if _, err := os.Stat(migrationsPath); err == nil {
+			return migrationsPath, nil
+		}
+
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			break
+		}
+		dir = parentDir
+	}
+
+	return "", fmt.Errorf("папка migrations не найдена")
+}
+
+func RunMigrate(name string) error {
+	migrationsPath, err := findMigrationsPath(name)
+	if err != nil {
+		return err
+	}
 	dbName := os.Getenv("DB_NAME")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -15,7 +42,9 @@ func RunMigrate(path string) error {
 	dbPort := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
-	cmd := exec.Command("migrate", "-database", dsn, "-path", path, "up")
+
+	cmd := exec.Command("migrate", "-database", dsn, "-path", migrationsPath, "up")
+	fmt.Println("Выполнение миграции:", cmd.String())
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
