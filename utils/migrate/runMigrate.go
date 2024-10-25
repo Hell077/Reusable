916c5292add4f5d723818ef093525881cec3b083
Hell077 <gdhald4@gmail.com>
2024-10-25 14:02:23 +0500

    update migrations function`s

--- utils/migrate/runMigrate.go
+++ utils/migrate/runMigrate.go
@@ -1,6 +1,6 @@
 /*
 This Go code implements a function that automatically searches for a migration folder across the entire project and runs database migrations in a separate goroutine.
-	The process is broken down into the following steps:
+The process is broken down into the following steps:
 
 1. **findMigrationFolder**: This function recursively searches the project directory for the folder containing migration files.
 	It uses the `filepath.Walk` function to traverse the file tree.
@@ -31,14 +31,17 @@ import (
 )
 
 func findMigrationFolder(folderName string) (string, error) {
+	log.Printf("Searching for migration folder: %s", folderName)
 	var folderPath string
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
+			log.Printf("Error during file walk: %v", err)
 			return err
 		}
 		if info.IsDir() && strings.Contains(path, folderName) {
 			folderPath = path
-			return filepath.SkipDir // Прерываем дальнейший поиск
+			log.Printf("Migration folder found at: %s", folderPath)
+			return filepath.SkipDir
 		}
 		return nil
 	})
@@ -53,28 +56,37 @@ func findMigrationFolder(folderName string) (string, error) {
 
 func Run(folderName string) {
 	go func() {
+		log.Println("Starting migration process...") // Log
+
 		dbName := os.Getenv("DB_NAME")
 		dbUser := os.Getenv("DB_USER")
 		dbPassword := os.Getenv("DB_PASSWORD")
 		dbHost := os.Getenv("DB_HOST")
 		dbPort := os.Getenv("DB_PORT")
 
+		log.Printf("Connecting to database %s on host %s:%s", dbName, dbHost, dbPort) // Log
+
 		dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
 
 		migrationsPath, err := findMigrationFolder(folderName)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error finding migration folder: %v", err) // Fatal log if error
 		}
 
 		migrationsFullPath := fmt.Sprintf("file://%s", migrationsPath)
+		log.Printf("Migrations path: %s", migrationsFullPath) // Log
 
 		if err := runMigrations(dbURL, migrationsFullPath); err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error running migrations: %v", err)
 		}
+
+		log.Println("Migrations applied successfully!")
 	}()
 }
 
 func runMigrations(dbURL, migrationsPath string) error {
+	log.Println("Initializing migration...") // Log
+
 	m, err := migrate.New(migrationsPath, dbURL)
 	if err != nil {
 		return fmt.Errorf("failed to initialize migration: %w", err)
@@ -84,5 +96,6 @@ func runMigrations(dbURL, migrationsPath string) error {
 		return fmt.Errorf("failed to apply migration: %w", err)
 	}
 
+	log.Println("Migration applied or already up to date") // Log success or no change
 	return nil
 }
